4cbc0f9ed53b8ba9b098f198f946b9b6c381d112
Valeria Oleynik <oleynik.valeria27@gmail.com>
2024-03-28 16:36:06 +0200

    make build

---
+++ index.4a1f60c7.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(t){var e=document.querySelector(".wall"),l=document.querySelector(".spider");if(t.target.closest(".wall")){var i=e.getBoundingClientRect(),c={top:t.clientY-i.top-e.clientTop-l.clientHeight/2,left:t.clientX-i.left-e.clientLeft-l.clientWidth/2};c.top<0&&(c.top=0),c.left<0&&(c.left=0),c.left+l.clientWidth>e.clientWidth&&(c.left=e.clientWidth-l.clientWidth),c.top+l.clientHeight>e.clientHeight&&(c.top=e.clientHeight-l.clientHeight),l.style.left=c.left+"px",l.style.top=c.top+"px"}});
+//# sourceMappingURL=index.4a1f60c7.js.map
